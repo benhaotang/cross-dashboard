3f0ab35ed1f29ac857c0a424975026baa23b8de0
benhaotang <benhaotang@outlook.com>
2026-02-24 10:48:53 +0100

    feat:removeiosaddappimage

---
+++ electron/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 600,
+    minHeight: 480,
+    autoHideMenuBar: true,
+    title: 'Cross Dashboard',
+    icon: path.join(__dirname, '../assets/icon.png'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, '../dist/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
