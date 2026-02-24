ae4e1088c5f876e6e6d4e34a9c672aeadf760d4d
benhaotang <benhaotang@outlook.com>
2026-02-24 22:26:02 +0100

    chore:version

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1354,7 +1354,7 @@ export default function SettingsScreen() {
       <View style={[styles.section, { backgroundColor: c.surface }]}>
         <Text style={[styles.sectionTitle, { color: c.text }]}>About</Text>
 
-        <Text style={[styles.aboutVersion, { color: c.text }]}>Cross-Dashboard v1.5.0</Text>
+        <Text style={[styles.aboutVersion, { color: c.text }]}>Cross-Dashboard v1.5.1</Text>
 
         <TouchableOpacity
           style={styles.aboutLink}
